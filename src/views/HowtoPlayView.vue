c57947846945e214f8e08b0e41074ac9a6a89cd4
Aaron Floreani <aaronfloreani@gmail.com>
2023-07-12 13:41:54 -0700

    added difficulties

--- src/views/HowtoPlayView.vue
+++ src/views/HowtoPlayView.vue
@@ -21,7 +21,7 @@
         <h1 class="text-3xl font-semibold mb-6">HOW TO PLAY</h1>
         <ol class="mb-6 list-decimal list-inside">
           <li>Select a song from the tracklist</li>
-          <li>Type the highlighted word on the right in the input box on the left</li>
+          <li>Type the highlighted word in the input box on the left</li>
           <li>Once you've typed the whole word, press the spacebar to submit the word</li>
           <li>If it's correct, your word score will increase; be careful punctuation and capitalization matter</li>
           <li>You must try to type all the words in each line of the song before the next line begins</li>
